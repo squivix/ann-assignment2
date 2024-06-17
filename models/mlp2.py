696ef24b6cecdb938e68d78d7e46690594832997
devintheamateurdevin <devintheamateurdevin@gmail.com>
2024-06-17 20:56:51 +0200

    progress with mlp model 2

--- models/mlp2.py
+++ models/mlp2.py
@@ -2,10 +2,16 @@ from torch import nn
 
 
 class MlpModel2(nn.Module):
-    def __init__(self, input_dim, output_dim, *args, **kwargs):
+    def __init__(self, input_dim_w, input_dim_h, output_dim, drop_out_percentage=0.1, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.model = nn.Sequential(
-
+            nn.Flatten(),
+            nn.Linear(input_dim_w * input_dim_h, 30),
+            nn.ReLU(),
+            nn.Dropout(drop_out_percentage),
+            nn.Linear(30, 30),
+            nn.ReLU(),
+            nn.Linear(30, output_dim),
         )
 
     def forward(self, x):
