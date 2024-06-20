e791c9bba994f2b0cba1d656e61445e390d22ff1
squivix <devintheamateurdevin@gmail.com>
2024-06-20 15:27:59 +0200

    more tweaking architecture

--- models/cnn2.py
+++ models/cnn2.py
@@ -7,17 +7,21 @@ class CnnModel2(nn.Module):
     def __init__(self, output_dim, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.model = nn.Sequential(
-            # nn.Conv2d(1, 16, kernel_size=5, stride=3, padding=1),
-            # nn.ReLU(),
-            # nn.AvgPool2d(kernel_size=2, stride=2),
             ResidualBlock(1, 16),
             ResidualBlock(16, 16),
-            # ResidualBlock(32, 32),
-            nn.Conv2d(16, 16, kernel_size=11, stride=3, padding="valid"),
-            nn.AvgPool2d(kernel_size=3, stride=1),
+            ResidualBlock(16, 16),
+            ResidualBlock(16, 16),
+            ResidualBlock(16, 16),
+            ResidualBlock(16, 16),
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding="valid"),
+            nn.ReLU(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding="valid"),
+            nn.ReLU(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
 
             nn.Flatten(start_dim=1),
-            nn.Linear(256, 200),
+            nn.Linear(400, 200),
             nn.BatchNorm1d(200),
             nn.ReLU(),
             nn.Dropout(0.25),
@@ -26,12 +30,10 @@ class CnnModel2(nn.Module):
             nn.ReLU(),
             nn.Dropout(0.25),
             nn.Linear(100, 50),
+            nn.BatchNorm1d(50),
             nn.ReLU(),
             nn.Dropout(0.25),
-            nn.Linear(50, 20),
-            nn.ReLU(),
-            nn.Dropout(0.25),
-            nn.Linear(20, output_dim),
+            nn.Linear(50, output_dim),
         )
 
     def forward(self, x):
